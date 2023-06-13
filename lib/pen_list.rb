41edd60fb9726f48b16001d6becfeb81218c669d
Tyler Elliott <tylercal@gmail.com>
2023-06-13 11:21:08 -0700

    make wordpress filters more specific

--- lib/pen_list.rb
+++ lib/pen_list.rb
@@ -24,7 +24,8 @@ module PenList
           path = req.path.downcase
           path.include?('/etc/passwd') ||
             path.include?('wordpress') ||
-            path.include?('/wp/') ||
+            path.include?('wp-content') ||
+            path.include?('wp-include') ||
             path.include?('/bc/') ||
             path.include?('/bk/') ||
             path.include?('old') ||
