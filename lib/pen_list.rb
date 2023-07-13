df3ce43412d60d895566541f2e9e5583509e5f4b
Tyler Elliott <tylercal@gmail.com>
2023-07-13 12:24:51 -0700

    organize exclude list

--- lib/pen_list.rb
+++ lib/pen_list.rb
@@ -23,42 +23,59 @@ module PenList
         Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
           path = req.path.downcase
           path.include?('/etc/passwd') ||
+            path.include?('/bc/') ||
+            path.include?('/bk/') ||
+            path.include?('/data/') ||
+            path.include?('/dist/') ||
+            path.include?('/feed/') ||
+            path.include?('/js/') ||
+            path.include?('/resources/') ||
+            path.include?('/static/') ||
+
+            path.include?('/blog') ||
+            path.include?('/api') ||
+
+            path.include?('admin') ||
+            path.include?('ajax') ||
+            path.include?('backup') ||
+            path.include?('catalog') ||
+            path.include?('cgi-bin') ||
+            path.include?('credentials') ||
+            path.include?('config') ||
+            path.include?('etc') ||
+            path.include?('exporttool') ||
+            path.include?('getkdata') ||
+            path.include?('git') ||
+            path.include?('login_index') ||
+            path.include?('login.htm') ||
+            path.include?('management') ||
+            path.include?('meta-inf') ||
+            path.include?('old') ||
+            path.include?('php') ||
+            path.include?('public') ||
+            path.include?('server-status') ||
+            path.include?('servlet') ||
+            path.include?('telerik') ||
+            path.include?('test_404_page') ||
+            path.include?('the_ins_ru') ||
             path.include?('wordpress') ||
             path.include?('wp-content') ||
             path.include?('wp-include') ||
-            path.include?('/bc/') ||
-            path.include?('/bk/') ||
-            path.include?('/api/') ||
-            path.include?('/blog/') ||
-            path.include?('old') ||
-            path.include?('admin') ||
-            path.include?('backup') ||
-            path.include?('cgi-bin') ||
-            path.include?('servlet') ||
-            path.include?('php') ||
-            path.include?('admin') ||
-            path.include?('test_404_page') ||
-            path.include?('etc') ||
-            path.include?('git') ||
-            path.include?('telerik') ||
-            path.include?('the_ins_ru') ||
-            path.include?('credentials') ||
-            path.include?('meta-inf') ||
-            path.include?('exporttool') ||
-            path.include?('server-status') ||
-            path.include?('public') ||
-            path.include?('catalog') ||
-            path.include?('config') ||
-            path.end_with?('.jsp') ||
+
+            path.end_with?('.cfc') ||
+            path.end_with?('.do') ||
             path.end_with?('.env') ||
             path.end_with?('.exe') ||
-            path.end_with?('.txt') ||
-            path.end_with?('.cfc') ||
-            path.end_with?('.xml') ||
+            path.end_with?('.html') ||
             path.end_with?('.gz') ||
+            path.end_with?('.jsp') ||
             path.end_with?('.sql') ||
+            path.end_with?('.txt') ||
+            path.end_with?('.xml') ||
+
             path.start_with?('/.') ||
             path.start_with?('/_') ||
+
             req.get? && path == '/auth/google_oauth2' ||
             req.post? && path == '/' # || don't forget the OR when adding more conditions
         end
